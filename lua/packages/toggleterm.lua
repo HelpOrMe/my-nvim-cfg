1e196f1a71df6c3e75d0bb09d32b61cccaeee520
HelpOrMe <helporme.dev@gmail.com>
2022-01-28 01:33:00 +0200

    Bind exit to the normal mode in the terminal

--- lua/packages/toggleterm.lua
+++ lua/packages/toggleterm.lua
@@ -3,3 +3,11 @@ load('toggleterm').setup {
     direction = 'vertical',
     size = vim.o.columns * 0.3
 }
+
+function _G.set_terminal_keymaps()
+  local opts = {noremap = true}
+  vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
+end
+
+
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
